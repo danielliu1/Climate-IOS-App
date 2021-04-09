71016693421882919c4d4684d2dd50649b03fb63
danielliu1 <dliu1.dev@gmail.com>
2021-04-09 03:07:56 -0700

    Network

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -6,6 +6,31 @@ struct WeatherManager {
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        performRequest(urlString: urlString)
+    }
+    
+    //When search is clicked // networking sections
+    func performRequest(urlString: String) {
+        //create URL
+        if let url = URL(string: urlString) {
+            //create URL Session
+            let session = URLSession(configuration: .default)
+            //give session task
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error: ))
+            
+            //start task
+            task.resume()
+        }
+    }
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
     }
 }
